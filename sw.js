16dc73f1a442fbfc33cde16c856a3d1e5f59fdc7
alexbasila <csac6316@gmail.com>
2025-08-14 10:51:45 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,16 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('hello-world-cache').then((cache) => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.webmanifest'
-      ]);
-    })
-  );
-});
+// Minimaler SW: reicht für Installierbarkeit
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (evt) => evt.waitUntil(self.clients.claim()));
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+// (optional) sehr einfacher Cache, nicht zwingend nötig:
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(
+    caches.open('hello-world-cache').then(async cache => {
+      const hit = await cache.match(evt.request, { ignoreSearch: true });
+      if (hit) return hit;
+      const res = await fetch(evt.request).catch(() => null);
+      if (res && res.ok && evt.request.method === 'GET') cache.put(evt.request, res.clone());
+      return res || new Response('offline', { status: 503 });
     })
   );
 });
